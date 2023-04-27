9fb4bc46b86ffd615adf92e00fe2e0c4a0726453
alyonaveselkova <131947886+fiona18378@users.noreply.github.com>
2023-04-27 10:11:27 +0300

    Add files via upload

---
+++ _ya-tr-span data-index=_1-0_ data-translated=_false_ data-source-lang=_en_ data-target-lang=_ru_ data-value=_main (1).py
@@ -0,0 +1,12 @@
+import random
+
+a = "abcdefghijklmnopqrstuvwxz"
+b = "ABCDEFJHIJKLMNOPQRSTUVWXYZ"
+c = "0123456789"
+d = "{}[]()*/,_-!?"
+
+all = a + b + c + d
+length = 16
+password = "".join(random.sample(all,length))
+print(password)
+input()
